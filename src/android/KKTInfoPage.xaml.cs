9b6c505c61dff2d79111d15c889900b74d1b16a3
RD AAOW Free development lab <adslbarxatov@gmail.com>
2026-02-19 01:36:11 +1100

    Assembly v 8.0 (19.02.26)

---
+++ src/android/KKTInfoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace RD_AAOW
+	{
+	/// <summary>
+	/// Класс описывает страницу настроек программы
+	/// </summary>
+	[XamlCompilation (XamlCompilationOptions.Compile)]
+	public partial class KKTInfoPage: ContentPage
+		{
+		/// <summary>
+		/// Конструктор. Запускает страницу
+		/// </summary>
+		public KKTInfoPage ()
+			{
+			InitializeComponent ();
+			}
+		}
+	}
---
+++ src/android/KKTSettingsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace RD_AAOW
+	{
+	/// <summary>
+	/// Класс описывает страницу настроек программы
+	/// </summary>
+	[XamlCompilation (XamlCompilationOptions.Compile)]
+	public partial class KKTSettingsPage: ContentPage
+		{
+		/// <summary>
+		/// Конструктор. Запускает страницу
+		/// </summary>
+		public KKTSettingsPage ()
+			{
+			InitializeComponent ();
+			}
+		}
+	}
